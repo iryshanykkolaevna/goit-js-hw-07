272d5a4e6af5cd69c71fbb84a7d13ff74220b2fe
iryshanykkolaevna <141450700+iryshanykkolaevna@users.noreply.github.com>
2023-12-17 20:45:13 +0200

    chainge

--- js/task-04.js
+++ js/task-04.js
@@ -1,22 +1,14 @@
-const registerForm = document.querySelector('.login-form');
-const btn = document.querySelector('.submit');
+const form = document.querySelector('.login-form');
 
-
-
-function handleSubmit(event) {
+form.addEventListener('submit', event => {
   event.preventDefault();
-  const form = event.target;
-  const email = form.elements.email.value;
-  const password = form.elements.password.value;
-
-  if (email === '' || password === '') {
-    return alert('Please fill in all the fields!');
-  } else {
-    const data = {
-      email: email.trim(),
-      password: password.trim(),
-    };
-      registerForm.reset();
-      console.log(data);
+  const data = {
+    email: event.target.elements.email.value.trim(),
+    password: event.target.elements.password.value.trim(),
+  };
+  if (data.email === '' || data.password == '') {
+    return alert('All form fields must be filled in');
   }
-}
+  console.log(data);
+  event.target.reset();
+});
